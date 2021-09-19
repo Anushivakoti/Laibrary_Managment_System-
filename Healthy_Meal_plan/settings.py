846e3a24196b61be9d8ae87cbd9361d1cffd0f96
Anushivakoti <shivakotianu2@gmail.com>
2021-09-19 15:44:40 +0545

    New updates in mealplan

--- Healthy_Meal_plan/settings.py
+++ Healthy_Meal_plan/settings.py
@@ -81,8 +81,16 @@ WSGI_APPLICATION = 'Healthy_Meal_plan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foods',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST' : 'localhost',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+
     }
 }
 
